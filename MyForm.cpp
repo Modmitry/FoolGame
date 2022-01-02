f1c4ee0a6d5686cdd6b2bbc3fba4e09b74f19d52
Dmitr <dmitriimoloshkin@yandex.ru>
2022-01-02 20:05:10 +0300

    22

--- MyForm.cpp
+++ MyForm.cpp
@@ -5,6 +5,5 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::Run(gcnew MyForm);
-
 	return 0;
 }
